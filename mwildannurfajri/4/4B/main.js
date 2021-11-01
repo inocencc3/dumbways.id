26c400f43adc79df4cb7c87b27f1b347b609dd91
inocencc3 <58712589+inocencc3@users.noreply.github.com>
2021-11-01 19:29:41 +0700

    Add files via upload

---
+++ mwildannurfajri/4/4B/main.js
@@ -0,0 +1,53 @@
+const btns = document.querySelectorAll('.btn');
+const items = document.querySelector('.item');
+
+const arrDummy = [{
+    id:1,
+    titleCourse:'PHP',
+    authorCourse:'Joker',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:2,
+    titleCourse:'HTML',
+    authorCourse:'Brandon',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:3,
+    titleCourse:'SASS',
+    authorCourse:'Cornelia',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:4,
+    titleCourse:'CSS',
+    authorCourse:'Pokimane',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:5,
+    titleCourse:'React Native',
+    authorCourse:'Tarik',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:6,
+    titleCourse:'Javascript',
+    authorCourse:'Shroud',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:7,
+    titleCourse:'Node.js',
+    authorCourse:'Tsubame',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},{
+    id:8,
+    titleCourse:'Laravel',
+    authorCourse:'Kaede',
+    textDesc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ducimus, nulla ea laboriosam necessitatibus accusantium.'
+},];
+let array_number = 0;
+
+window.addEventListener('DOMContentLoaded',()=>{
+    arrDummy.forEach((val)=>{
+        items.innerHTML = ' ';
+        // const x = val;
+    })
+})
+
